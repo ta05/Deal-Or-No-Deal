14439ea85fb957c8efdb594046b71cb2464776b2
Tolu Alimi <tolualimi05@gmail.com>
2020-05-21 16:15:53 -0500

    Added variables for totalcasesopened and moneyvaluesremaining. Added a function for calculating standard deviation and a function for the game flow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,55 @@
-var moneyList = [0.01,1,5,10,25,50,75,100,200,300,400,500,750,1000,5000,10000,25000,50000,75000,100000,200000,300000,400000,500000,750000, 1000000];
-var casesOpened;
+var moneyList = [0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000];
+var moneyValuesRemaining;
+var totalCasesOpened;
 var totalCases;
 var remainingMoney;
-var roundNum;
-var numCasesOpenedPerRound = [6, 5, 4, 3, 2, 1, 1, 1, 1];
+var round;
+var casesOpenedThisRound;
+var numCasesOpenedPerRound = {
+    1: 6,
+    2: 5,
+    3: 4,
+    4: 3,
+    5: 2,
+    6: 1,
+    7: 1,
+    8: 1,
+    9: 1
+};
 var gameState;
 var hasPlayerSelectedCase;
 
 initialize();
 
-switch (gameState) {
-    case 0:
-        $(".case").click(function () {
-            if (!hasPlayerSelectedCase) {
-                var $selectedCase = $(this);
-                pickMyCase($selectedCase);
-                hasPlayerSelectedCase = true;
+function gameFlow() {
+    switch (gameState) {
+        case 0:
+            while (!hasPlayerSelectedCase) {
+                $(".case").click(function () {
+                    var $selectedCase = $(this);
+                    pickMyCase($selectedCase);
+                    hasPlayerSelectedCase = true;
+                });
             }
-        });
-        gameState = 1;
-        break;
-};
+            gameState = 1;
+            gameFlow();
+            break;
+        case 1:
+            while (casesOpenedThisRound < numCasesOpenedPerRound[round]) {
+                $(".case").click(".not-clicked", function () {
+                    var $selectedCase = $(this);
+                    selectCase($selectedCase);
+                });
+            }
+            gameState = 2;
+            
+    };
+}
 
 function initialize() {
     createMoneyTable();
     assignCaseAmounts();
+    moneyValuesRemaining = moneyList.slice();
     gameState = 0;
     hasPlayerSelectedCase = false;
 }
@@ -42,7 +67,7 @@ function createMoneyTable() {
 }
 
 function assignCaseAmounts() {
-    casesOpened = 0;
+    totalCasesOpened = 0;
     totalCases = moneyList.length;
     remainingMoney = calcTotalMoneyAmount();
 
@@ -60,16 +85,30 @@ function assignCaseAmounts() {
 function pickMyCase(el) {
     $("#your-case").addClass("chosen-case").text($(el).text()).val($(el).val());
     $(el).removeClass("not-clicked").addClass("players-case");
+    round = 1;
 }
 
 function selectCase(el) {
-    casesOpened++;
+    totalCasesOpened++;
+    casesOpenedThisRound++;
     remainingMoney -= parseFloat($(el).val());
+    moneyValuesRemaining.splice(moneyValuesRemaining.splice($(el).val(), 1));
     $(el).removeClass("not-clicked").addClass("selected-case");
 }
 
 function calcExpectedValue() {
-    return remainingMoney / (totalCases - casesOpened);
+    return remainingMoney / (totalCases - totalCasesOpened);
+}
+
+function calcEX2() {
+    var ex2 = 0;
+    for (var i = 0; i < moneyValuesRemaining.length; i++)
+        ex2 += Math.pow(moneyValuesRemaining[i], 2);
+    return ex2;
+}
+
+function calcStandardDeviation() {
+    return Math.pow(calcExpectedValue(), 2) - calcEX2();
 }
 
 function calcTotalMoneyAmount() {
