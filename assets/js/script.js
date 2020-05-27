f80f3371f6dffd2eb9c7c459c221effe42e8ec3c
Tolu Alimi <tolualimi05@gmail.com>
2020-05-27 14:06:18 -0500

    Finished the bank offer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,4 @@
 var moneyList = [0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000];
-var moneyValuesRemaining;
-var totalCasesOpened;
-var totalCases;
-var remainingMoney;
-var round;
-var casesOpenedThisRound;
 var numCasesOpenedPerRound = {
     1: 6,
     2: 5,
@@ -16,27 +10,38 @@ var numCasesOpenedPerRound = {
     8: 1,
     9: 1
 };
+
+var bankersOfferMeanSD = {
+    1: [22.485, 8.385],
+    2: [33.685,11.232],
+    3: [44.87, 8.740],
+    4: [52.46, 13.50],
+    5: [63.745, 14.002],
+    6: [72.75, 12.719],
+    7: [74.98, 17.622],
+    8: [80.92, 17.260],
+    9: [78.88, 15.321]
+};
+
+var moneyValuesRemaining;
+var totalCasesOpened;
+var totalCases;
+var remainingMoney;
+var round;
+var casesOpenedThisRound;
 var hasPlayerSelectedCase;
+var lastOffer;
+var counterOffer;
 
 initialize();
 
 
 function initialize() {
     createMoneyTable();
+    assignVariables();
     assignCaseAmounts();
-    moneyValuesRemaining = moneyList.slice();
-    hasPlayerSelectedCase = false;
-}
 
-function gameFlow() {
-    $(".case").click(function () {
-        if (!hasPlayerSelectedCase) {
-            var $selectedCase = $(this);
-            pickMyCase($selectedCase);
-            hasPlayerSelectedCase = true;
-            gameFlow();
-        }
-    });
+    selectPlayersCase();
 }
 
 function createMoneyTable() {
@@ -51,38 +56,84 @@ function createMoneyTable() {
     }
 }
 
-function assignCaseAmounts() {
+function assignVariables() {
+    moneyValuesRemaining = moneyList.slice();
     totalCasesOpened = 0;
     totalCases = moneyList.length;
     remainingMoney = calcTotalMoneyAmount();
+    round = 0;
+    casesOpenedThisRound = 0;
+    hasPlayerSelectedCase = false;
+}
 
+function assignCaseAmounts() {
     var values = moneyList.slice();
     for (var i = 26; i >= 1; i--){
         var randNum = Math.floor(Math.random() * values.length);
         var caseValue = values[randNum];
 
-        $("#case-" + (i)).attr("value", caseValue);
+        $("#case-" + (i)).val(caseValue);
 
         values.splice(values.indexOf(caseValue), 1);
     }
 }
 
-function pickMyCase(el) {
+function displayMyCase(el) {
     $("#your-case").addClass("chosen-case").text($(el).text()).val($(el).val());
     $(el).removeClass("not-clicked").addClass("players-case");
-    round = 1;
 }
 
-function selectCase(el) {
+function removeSelectedCase(el) {
     totalCasesOpened++;
     casesOpenedThisRound++;
     remainingMoney -= parseFloat($(el).val());
-    moneyValuesRemaining.splice(moneyValuesRemaining.splice($(el).val(), 1));
+    moneyValuesRemaining.splice(moneyValuesRemaining.indexOf(parseFloat($(el).val())), 1);
+    console.log("Case Opened: $" + formatNumber(parseFloat($(el).val())));
     $(el).removeClass("not-clicked").addClass("selected-case");
 }
 
+function selectPlayersCase() {
+    $(".case").click(function () {
+        if (!hasPlayerSelectedCase) {
+            var $selectedCase = $(this);
+            displayMyCase($selectedCase);
+            hasPlayerSelectedCase = true;
+            round++;
+            openCase();
+        }
+    });
+}
+
+function openCase() {
+    $(".case").click("not-clicked", function () {
+        if (casesOpenedThisRound < numCasesOpenedPerRound[round]) {
+            var $selectedCase = $(this);
+            removeSelectedCase($selectedCase);
+            console.log("Number of Cases Opened This Round: " + casesOpenedThisRound);
+            if (casesOpenedThisRound === numCasesOpenedPerRound[round])
+                bankersOffer();
+        }
+    });
+}
+
 function bankersOffer() {
-    var offer = Math.round((0.75 * calcExpectedValue()) / 1000) * 1000;
+    var mean = bankersOfferMeanSD[round][0];
+    var sd = bankersOfferMeanSD[round][1];
+
+    var pvalue = Math.random() * 0.95 - 0.475;
+    var z = percentile_z(pvalue);
+    var ex = calcExpectedValue();
+    var pi = ((z * sd) + mean);
+
+    lastOffer = Math.round((0.01*pi * ex) / 1000) * 1000;
+
+    console.log("P-value: " + pvalue);
+    console.log("z: " + z);
+    console.log("Expected Value: " + ex);
+    console.log("Pi: " + pi);
+
+    console.log("Banker's Offer: $" + formatNumber(lastOffer));
+    return lastOffer;
 }
 
 function calcExpectedValue() {
@@ -111,4 +162,20 @@ function formatNumber(num) {
     if(num>1)
         return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return num;
+}
+
+function percentile_z(p) {
+    var a0= 2.5066282,  a1=-18.6150006,  a2= 41.3911977,   a3=-25.4410605,
+        b1=-8.4735109,  b2= 23.0833674,  b3=-21.0622410,   b4=  3.1308291,
+        c0=-2.7871893,  c1= -2.2979648,  c2=  4.8501413,   c3=  2.3212128,
+        d1= 3.5438892,  d2=  1.6370678, r, z;
+
+    if (p>0.42) {
+        r=Math.sqrt(-Math.log(0.5-p));
+        z=(((c3*r+c2)*r+c1)*r+c0)/((d2*r+d1)*r+1);
+    } else {
+        r=p*p;
+        z=p*(((a3*r+a2)*r+a1)*r+a0)/((((b4*r+b3)*r+b2)*r+b1)*r+1);
+    }
+    return z;
 }
